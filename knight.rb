c58d64a9fe6eda42afae17ff337f9f20b291ffdd
Yining <ynzhang23@gmail.com>
2022-12-24 17:26:12 +0800

    Add methods to generate paths and more
    Returns the last position on the shortest path before reaching the
    destination.

--- knight.rb
+++ knight.rb
@@ -1,67 +1,81 @@
 # frozen_string_literal: true
 
+require_relative 'board'
 require 'pry-byebug'
 
 # Creates a knight piece
 class Knight
-  attr_reader :position, :next_position
-
-  def initialize(position)
-    @position = position
-    @next_position = generate_next_positions(position)
+  def initialize(start_pos, end_pos)
+    @start = Board.new(start_pos)
+    @end = Board.new(end_pos)
+    # All Nodes
+    @travelled = []
+    # Tracking Arrays
+    @next_step = []
+    @latest_positions = []
+    @landed = false
   end
 
-  def valid_position?(new_x, new_y)
-    return false if new_x < 0 || new_x > 8 || new_y < 0 || new_y > 8
-    true
+  # Checks if the next move is the destination
+  def landed_on_destination(pos)
+    pos == @end.position
   end
 
-  def add_to_possible_positions(new_x, new_y, possible_positions)
-    temp_position = [new_x, new_y]
-    possible_positions.push(temp_position.dup)
-    temp_position.clear
-    possible_positions
+  # First move
+  def first_move
+    @latest_positions << @start.position
+
+    # Add current positions' next steps to queue and track travelled node to graph
+    @latest_positions.each do |pos|
+      # Check and return previous step if current positions are the destination
+      if landed_on_destination(pos)
+        puts pos.to_s
+        return @landed = true
+      end
+      @travelled << Board.new(pos)
+      @next_step.push(*Board.new(pos).connected)
+    end
+    # Reset queue
+    @latest_positions.clear
   end
 
-  def generate_next_positions(position)
-    possible_positions = []
+  # Subsequent moves
+  def next_move
+    @latest_positions = @next_step.dup
+    @next_step.clear
 
-    new_x = position[0] + 2
-    new_y = position[1] + 1
-    add_to_possible_positions(new_x, new_y, possible_positions) if valid_position?(new_x, new_y)
+    binding.pry
+    # Loop across the current positions
+    @latest_positions.each do |pos|
+      # Check and return previous step if current positions are the destination
+      if landed_on_destination(pos)
+        puts pos.to_s
+        return @landed = true
+      end
+      # Create and add vertice to graph
+      @travelled << Board.new(pos)
+      # Add next steps from current step
+      @next_step.push(*Board.new(pos).connected)
+    end
+    # Filter next steps to prevent re-travel
+    @next_step.reject! {|pos| extract_positions.include?(pos)}
+    @latest_positions.clear
+  end
 
-    new_x = position[0] + 2
-    new_y = position[1] - 1
-    add_to_possible_positions(new_x, new_y, possible_positions) if valid_position?(new_x, new_y)
+  # Return positions from all vertices
+  def extract_positions
+    temp_array = []
+    @travelled.each do |position_node|
+      temp_array << position_node.position 
+    end
+    temp_array
+  end
 
-    new_x = position[0] + 1
-    new_y = position[1] + 2
-    add_to_possible_positions(new_x, new_y, possible_positions) if valid_position?(new_x, new_y)
-
-    new_x = position[0] + 1
-    new_y = position[1] - 2
-    add_to_possible_positions(new_x, new_y, possible_positions) if valid_position?(new_x, new_y)
-
-    new_x = position[0] - 2
-    new_y = position[1] + 1
-    add_to_possible_positions(new_x, new_y, possible_positions) if valid_position?(new_x, new_y)
-
-    new_x = position[0] - 2
-    new_y = position[1] - 1
-    add_to_possible_positions(new_x, new_y, possible_positions) if valid_position?(new_x, new_y)
-
-    new_x = position[0] - 1
-    new_y = position[1] + 2
-    add_to_possible_positions(new_x, new_y, possible_positions) if valid_position?(new_x, new_y)
-
-    new_x = position[0] - 1
-    new_y = position[1] - 2
-    add_to_possible_positions(new_x, new_y, possible_positions) if valid_position?(new_x, new_y)
-
-    possible_positions
+  def create_path
+    first_move
+    next_move until @landed == true
   end
 end
 
-
-knight = Knight.new([0, 0])
-p knight.next_position
+knight = Knight.new([4, 3], [2, 3])
+knight.create_path
